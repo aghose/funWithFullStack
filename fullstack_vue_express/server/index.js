1e061c4076229a71533c3ba9d5d517399a71519b
aghose <aghose@buffalo.edu>
2020-09-14 05:17:10 -0400

    I don't know

--- fullstack_vue_express/server/index.js
+++ fullstack_vue_express/server/index.js
@@ -14,6 +14,18 @@ const posts = require('./routes/api/posts');
 //Any route that uses api/posts will be rerouted to posts
 app.use('/api/posts', posts);
 
+//Handle production
+if(process.env.NODE_ENV === 'production') {
+    
+    //Static folder
+    app.use(express.static(__dirname + '/public'));
+
+    //Handle single page application
+    app.get(/.*/, (req, res) => res.sendFile(__dirname + 
+        '/public/index.html'));
+
+}
+
 //Port will be process.env.PORT in Heroku or 5000 for local port
 const port = process.env.PORT || 5000;
 
